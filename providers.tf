64e5a05a1ac7855e4e8332c6e17b66e1a01fc9ad
lekshmichandrans10 <34676672+lekshmichandrans10@users.noreply.github.com>
2019-12-02 18:15:38 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   version = "~>1.25"
-  subscription_id = "1d021829-09c2-47de-9160-f9597e6f66ad"
-  client_id       = "2ff773c2-cfa8-49d3-b007-39a118bf0c6e"
-  client_secret   = "cd584b10-cc33-496d-8d93-dfc485f3b2da"
-  tenant_id       = "6e06e42d-6925-47c6-b9e7-9581c7ca302a"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 }
 
 terraform {
